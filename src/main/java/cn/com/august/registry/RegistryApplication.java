6d33c6b5ce83668d9f83626647b0dc69a506387c
ashangdiing <48668192+ashangdiing@users.noreply.github.com>
2019-06-13 13:37:08 +0800

    提交

--- src/main/java/cn/com/august/registry/RegistryApplication.java
+++ src/main/java/cn/com/august/registry/RegistryApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
